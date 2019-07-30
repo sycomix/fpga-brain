86b7aac1952b519e3b74451e0584290fde0ee792
rgonzalez <3droberto@gmail.com>
2019-07-30 03:37:28 +0200

    net inputs & weights signals

--- fpgabrain.vhdl
+++ fpgabrain.vhdl
@@ -1,5 +1,6 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.all; 
+USE ieee.numeric_std.all;
 
 LIBRARY work;
 
@@ -113,14 +114,181 @@ COMPONENT net
 	);
 END COMPONENT;
 
+-- PLL
 SIGNAL	CLK_VGA :  STD_LOGIC;
 SIGNAL	areset :  STD_LOGIC;
 SIGNAL	locked :  STD_LOGIC;
 
+-- NET
+SIGNAL	i0 :  STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	i1 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	i2 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	i3 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	i4 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wi0_h0 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wi0_h1 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wi0_h2 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wi0_h3 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wi0_h4 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wi1_h0 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wi1_h1 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wi1_h2 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wi1_h3 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wi1_h4 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wi2_h0 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wi2_h1 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wi2_h2 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wi2_h3 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wi2_h4 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wi3_h0 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wi3_h1 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wi3_h2 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wi3_h3 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wi3_h4 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wi4_h0 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wi4_h1 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wi4_h2 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wi4_h3 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wi4_h4 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+
+SIGNAL	wh0_h5 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wh0_h6 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wh0_h7 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wh0_h8 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wh0_h9 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wh1_h5 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wh1_h6 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wh1_h7 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wh1_h8 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wh1_h9 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wh2_h5 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wh2_h6 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wh2_h7 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wh2_h8 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wh2_h9 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wh3_h5 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wh3_h6 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wh3_h7 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wh3_h8 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wh3_h9 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wh4_h5 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wh4_h6 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wh4_h7 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wh4_h8 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wh4_h9 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+
+SIGNAL	wh5_out0 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wh5_out1 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wh5_out2 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wh6_out0 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wh6_out1 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wh6_out2 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wh7_out0 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wh7_out1 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wh7_out2 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wh8_out0 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wh8_out1 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wh8_out2 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wh9_out0 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wh9_out1 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	wh9_out2 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+
+SIGNAL	out0 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	out1 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL	out2 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+
 BEGIN 
+i0 <= STD_LOGIC_VECTOR(to_signed(12345, i0'length));
+i1 <= STD_LOGIC_VECTOR(to_signed(12345, i1'length));
+i2 <= STD_LOGIC_VECTOR(to_signed(12345, i2'length));
+i3 <= STD_LOGIC_VECTOR(to_signed(12345, i3'length));
+i4 <= STD_LOGIC_VECTOR(to_signed(12345, i4'length));
+wi0_h0 <= STD_LOGIC_VECTOR(to_signed(12345, wi0_h0'length));
+wi0_h1 <= STD_LOGIC_VECTOR(to_signed(12345, wi0_h1'length));
+wi0_h2 <= STD_LOGIC_VECTOR(to_signed(12345, wi0_h2'length));
+wi0_h3 <= STD_LOGIC_VECTOR(to_signed(12345, wi0_h3'length));
+wi0_h4 <= STD_LOGIC_VECTOR(to_signed(12345, wi0_h4'length));
+wi1_h0 <= STD_LOGIC_VECTOR(to_signed(12345, wi1_h0'length));
+wi1_h1 <= STD_LOGIC_VECTOR(to_signed(12345, wi1_h1'length));
+wi1_h2 <= STD_LOGIC_VECTOR(to_signed(12345, wi1_h2'length));
+wi1_h3 <= STD_LOGIC_VECTOR(to_signed(12345, wi1_h3'length));
+wi1_h4 <= STD_LOGIC_VECTOR(to_signed(12345, wi1_h4'length));
+wi2_h0 <= STD_LOGIC_VECTOR(to_signed(12345, wi2_h0'length));
+wi2_h1 <= STD_LOGIC_VECTOR(to_signed(12345, wi2_h1'length));
+wi2_h2 <= STD_LOGIC_VECTOR(to_signed(12345, wi2_h2'length));
+wi2_h3 <= STD_LOGIC_VECTOR(to_signed(12345, wi2_h3'length));
+wi2_h4 <= STD_LOGIC_VECTOR(to_signed(12345, wi2_h4'length));
+wi3_h0 <= STD_LOGIC_VECTOR(to_signed(12345, wi3_h0'length));
+wi3_h1 <= STD_LOGIC_VECTOR(to_signed(12345, wi3_h1'length));
+wi3_h2 <= STD_LOGIC_VECTOR(to_signed(12345, wi3_h2'length));
+wi3_h3 <= STD_LOGIC_VECTOR(to_signed(12345, wi3_h3'length));
+wi3_h4 <= STD_LOGIC_VECTOR(to_signed(12345, wi3_h4'length));
+wi4_h0 <= STD_LOGIC_VECTOR(to_signed(12345, wi4_h0'length));
+wi4_h1 <= STD_LOGIC_VECTOR(to_signed(12345, wi4_h1'length));
+wi4_h2 <= STD_LOGIC_VECTOR(to_signed(12345, wi4_h2'length));
+wi4_h3 <= STD_LOGIC_VECTOR(to_signed(12345, wi4_h3'length));
+wi4_h4 <= STD_LOGIC_VECTOR(to_signed(12345, wi4_h4'length));
+
+wh0_h5 <= STD_LOGIC_VECTOR(to_signed(12345, wh0_h5'length));
+wh0_h6 <= STD_LOGIC_VECTOR(to_signed(12345, wh0_h6'length));
+wh0_h7 <= STD_LOGIC_VECTOR(to_signed(12345, wh0_h7'length));
+wh0_h8 <= STD_LOGIC_VECTOR(to_signed(12345, wh0_h8'length));
+wh0_h9 <= STD_LOGIC_VECTOR(to_signed(12345, wh0_h9'length));
+wh1_h5 <= STD_LOGIC_VECTOR(to_signed(12345, wh1_h5'length));
+wh1_h6 <= STD_LOGIC_VECTOR(to_signed(12345, wh1_h6'length));
+wh1_h7 <= STD_LOGIC_VECTOR(to_signed(12345, wh1_h7'length));
+wh1_h8 <= STD_LOGIC_VECTOR(to_signed(12345, wh1_h8'length));
+wh1_h9 <= STD_LOGIC_VECTOR(to_signed(12345, wh1_h9'length));
+wh2_h5 <= STD_LOGIC_VECTOR(to_signed(12345, wh2_h5'length));
+wh2_h6 <= STD_LOGIC_VECTOR(to_signed(12345, wh2_h6'length));
+wh2_h7 <= STD_LOGIC_VECTOR(to_signed(12345, wh2_h7'length));
+wh2_h8 <= STD_LOGIC_VECTOR(to_signed(12345, wh2_h8'length));
+wh2_h9 <= STD_LOGIC_VECTOR(to_signed(12345, wh2_h9'length));
+wh3_h5 <= STD_LOGIC_VECTOR(to_signed(12345, wh3_h5'length));
+wh3_h6 <= STD_LOGIC_VECTOR(to_signed(12345, wh3_h6'length));
+wh3_h7 <= STD_LOGIC_VECTOR(to_signed(12345, wh3_h7'length));
+wh3_h8 <= STD_LOGIC_VECTOR(to_signed(12345, wh3_h8'length));
+wh3_h9 <= STD_LOGIC_VECTOR(to_signed(12345, wh3_h9'length));
+wh4_h5 <= STD_LOGIC_VECTOR(to_signed(12345, wh4_h5'length));
+wh4_h6 <= STD_LOGIC_VECTOR(to_signed(12345, wh4_h6'length));
+wh4_h7 <= STD_LOGIC_VECTOR(to_signed(12345, wh4_h7'length));
+wh4_h8 <= STD_LOGIC_VECTOR(to_signed(12345, wh4_h8'length));
+wh4_h9 <= STD_LOGIC_VECTOR(to_signed(12345, wh4_h9'length));
+
+wh5_out0 <= STD_LOGIC_VECTOR(to_signed(12345, wh5_out0'length));
+wh5_out1 <= STD_LOGIC_VECTOR(to_signed(12345, wh5_out1'length));
+wh5_out2 <= STD_LOGIC_VECTOR(to_signed(12345, wh5_out2'length));
+wh6_out0 <= STD_LOGIC_VECTOR(to_signed(12345, wh6_out0'length));
+wh6_out1 <= STD_LOGIC_VECTOR(to_signed(12345, wh6_out1'length));
+wh6_out2 <= STD_LOGIC_VECTOR(to_signed(12345, wh6_out2'length));
+wh7_out0 <= STD_LOGIC_VECTOR(to_signed(12345, wh7_out0'length));
+wh7_out1 <= STD_LOGIC_VECTOR(to_signed(12345, wh7_out1'length));
+wh7_out2 <= STD_LOGIC_VECTOR(to_signed(12345, wh7_out2'length));
+wh8_out0 <= STD_LOGIC_VECTOR(to_signed(12345, wh8_out0'length));
+wh8_out1 <= STD_LOGIC_VECTOR(to_signed(12345, wh8_out1'length));
+wh8_out2 <= STD_LOGIC_VECTOR(to_signed(12345, wh8_out2'length));
+wh9_out0 <= STD_LOGIC_VECTOR(to_signed(12345, wh9_out0'length));
+wh9_out1 <= STD_LOGIC_VECTOR(to_signed(12345, wh9_out1'length));
+wh9_out2 <= STD_LOGIC_VECTOR(to_signed(12345, wh9_out2'length));
+
 
 c1 : pll PORT MAP(areset, CLK, CLK_VGA, locked);
 c2 : vga PORT MAP(CLK_VGA, VGA_HS, VGA_VS, VGA_R, VGA_G, VGA_B);
---c3 : net PORT MAP(CLK, );
+c3 : net PORT MAP(CLK, i0, i1, i2, i3, i4,  wi0_h0, wi0_h1, wi0_h2, wi0_h3, wi0_h4,
+											wi1_h0, wi1_h1, wi1_h2, wi1_h3, wi1_h4,
+											wi2_h0, wi2_h1, wi2_h2, wi2_h3, wi2_h4,
+											wi3_h0, wi3_h1, wi3_h2, wi3_h3, wi3_h4,
+											wi4_h0, wi4_h1, wi4_h2, wi4_h3, wi4_h4,
+											wh0_h5, wh0_h6, wh0_h7, wh0_h8, wh0_h9,
+											wh1_h5, wh1_h6, wh1_h7, wh1_h8, wh1_h9,
+											wh2_h5, wh2_h6, wh2_h7, wh2_h8, wh2_h9,
+											wh3_h5, wh3_h6, wh3_h7, wh3_h8, wh3_h9,
+											wh4_h5, wh4_h6, wh4_h7, wh4_h8, wh4_h9,
+											wh5_out0, wh5_out1, wh5_out2,
+											wh6_out0, wh6_out1, wh6_out2,
+											wh7_out0, wh7_out1, wh7_out2,
+											wh8_out0, wh8_out1, wh8_out2,
+											wh9_out0, wh9_out1, wh9_out2,
+											out0, out1, out2);
 
 END bhv;
