ceeb67503e7621b415cda72b7703833cadd608a2
rgonzalez <3droberto@gmail.com>
2019-08-07 14:32:17 +0200

    RAM progress

--- ram16.vhd
+++ ram16.vhd
@@ -8,26 +8,26 @@ ENTITY ram16 IS
 	(
 		CLK_IN: IN STD_LOGIC;
 		CKE: OUT STD_LOGIC:='0';
-		RA: OUT STD_LOGIC_VECTOR(12 DOWNTO 0);		
-		DQ: OUT STD_LOGIC_VECTOR(15 DOWNTO 0);
-		UMQM: OUT STD_LOGIC;
-		LDQM: OUT STD_LOGIC;
-		CS: OUT STD_LOGIC;
-		RAS: OUT STD_LOGIC;
-		CAS: OUT STD_LOGIC;
-		WE: OUT STD_LOGIC;
-		BA: OUT STD_LOGIC_VECTOR(1 DOWNTO 0);
+		RA: OUT STD_LOGIC_VECTOR(12 DOWNTO 0):="0000000000000";		
+		DQ: OUT STD_LOGIC_VECTOR(15 DOWNTO 0):="0000000000000000";
+		UMQM: OUT STD_LOGIC:='0';
+		LDQM: OUT STD_LOGIC:='0';
+		CS: OUT STD_LOGIC:='0';
+		RAS: OUT STD_LOGIC:='0';
+		CAS: OUT STD_LOGIC:='0';
+		WE: OUT STD_LOGIC:='0';
+		BA: OUT STD_LOGIC_VECTOR(1 DOWNTO 0):="00";
 		
 		ram_initialized: OUT STD_LOGIC := '0';
 		ram_row_addr: IN STD_LOGIC_VECTOR(12 DOWNTO 0);
 		ram_col_addr: IN STD_LOGIC_VECTOR(12 DOWNTO 0);
 		
 		ram_data_save: IN STD_LOGIC_VECTOR(15 DOWNTO 0);
-		ram_data_save_ready: OUT STD_LOGIC;
+		ram_data_save_ready: OUT STD_LOGIC:='0';
 		
 		ram_data_read_do: IN STD_LOGIC;
-		ram_data_read_ready: OUT STD_LOGIC;
-		ram_data_read: OUT STD_LOGIC_VECTOR(15 DOWNTO 0)
+		ram_data_read_ready: OUT STD_LOGIC:='0';
+		ram_data_read: OUT STD_LOGIC_VECTOR(15 DOWNTO 0):="0000000000000000"
 	);
 END ram16;
 
@@ -42,21 +42,32 @@ BEGIN
 	PROCESS(CLK_IN)
 	BEGIN
 		IF (rising_edge(CLK_IN)) THEN
-			IF CMD = 0 THEN -- POWER
+			-- http://codehackcreate.com/archives/444
+			IF CMD = 0 THEN -- POWER (200us) AND NOP (NO OPERATION COMMAND)
 				IF n_s < 28600 THEN
-					n_s <= n_s+1;
-				ELSE
-					CMD <= 1;
-					n_s <= 0;
-				END IF;
-			ELSIF CMD = 1 THEN	-- NO OPERATION
-				IF n_s < 1 THEN
 					CKE <= '1';
 					CS <= '0';
 					RAS <= '1';
 					CAS <= '1';
 					WE <= '1';
 					RA <= "0000000000000";
+					BA <= "00";
+					UMQM <= '1';
+					LDQM <= '1';
+					
+					n_s <= n_s+1;
+				ELSE
+					CMD <= 1;
+					n_s <= 0;
+				END IF;
+			ELSIF CMD = 1 THEN	-- PRECHARGE ALL BANKS 20ns tRP (RAS Precharge Time)
+				IF n_s < 3 THEN
+					CKE <= '1';
+					CS <= '0';
+					RAS <= '0';
+					CAS <= '1';
+					WE <= '0';
+					RA <= "0010000000000"; -- RA[10] := '1' Precharge all banks.
 					BA <= "00";
 				
 					n_s <= n_s+1;
@@ -64,14 +75,14 @@ BEGIN
 					CMD <= 2;
 					n_s <= 0;
 				END IF;		
-			ELSIF CMD = 2 THEN	-- PRECHARGE ALL BANKS
-				IF n_s < 1 THEN
+			ELSIF CMD = 2 THEN	-- REFRESH 63ns tRC (RAS Cycle Time) * 8 refresh cycles ====> 7.5ns/cycle => 8.4 => 9 cycles * 8 refresh cycles = 56 cycles
+				IF n_s < 56 THEN
 					CKE <= '1';
 					CS <= '0';
 					RAS <= '0';
-					CAS <= '1';
-					WE <= '0';
-					RA <= "0010000000000";
+					CAS <= '0';
+					WE <= '1';
+					RA <= "0000000000000";
 					BA <= "00";
 				
 					n_s <= n_s+1;
@@ -79,60 +90,30 @@ BEGIN
 					CMD <= 3;
 					n_s <= 0;
 				END IF;		
-			ELSIF CMD = 3 THEN	-- NO OPERATION
+			ELSIF CMD = 3 THEN	--  Mode Register Set command 
+				-- 0 RA12
+				-- 00 RA11-RA10
+				-- 0 RA9 (Write Mode = Burst Read and Burst Write)
+				-- 00 RA8-RA7
+				-- 010 RA6-RA4 (CAS Latency = 2)
+				-- 0 RA3 (Burst Type 0 = Sequential; 1 = Interleave)
+				-- 111 RA2-RA0 (Burst Length = 16 (Full))
 				IF n_s < 3 THEN
 					CKE <= '1';
 					CS <= '0';
-					RAS <= '1';
-					CAS <= '1';
-					WE <= '1';
-					RA <= "0000000000000";
+					RAS <= '0';
+					CAS <= '0';
+					WE <= '0';
+					RA <= "0"&"00"&"0"&"00"&"010"&"0"&"111";
 					BA <= "00";
 				
 					n_s <= n_s+1;
 				ELSE					
 					CMD <= 4;
 					n_s <= 0;
-				END IF;	
-			ELSIF CMD = 4 THEN	--  Mode Register Set command 
-				-- 0 RA12
-				-- 00 RA11-RA10
-				-- 0 RA9 (Write Mode = Burst Read and Burst Write)
-				-- 00 RA8-RA7
-				-- 010 RA6-RA4 (CAS Latency = 2)
-				-- 1 RA3 (Burst Type = Interleave)
-				-- 001 RA2-RA0 (Burst Length = 2)
-				IF n_s < 1 THEN
-					CKE <= '1';
-					CS <= '0';
-					RAS <= '0';
-					CAS <= '0';
-					WE <= '0';
-					RA <= "0000000101001";
-					BA <= "00";
-				
-					n_s <= n_s+1;
-				ELSE					
-					CMD <= 5;
-					n_s <= 0;
 				END IF;		
-			ELSIF CMD = 5 THEN	--  PRECHARGE ALL BANKS AGAIN
-				IF n_s < 28600 THEN
-					CKE <= '1';
-					CS <= '0';
-					RAS <= '0';
-					CAS <= '1';
-					WE <= '0';
-					RA <= "0010000000000";
-					BA <= "00";
-				
-					n_s <= n_s+1;
-				ELSE					
-					CMD <= 6;
-					n_s <= 0;
-				END IF;		
-			ELSIF CMD = 6 THEN	--  AUTOREFRESH
-				IF n_s < 1 THEN
+			ELSIF CMD = 4 THEN	--  REFRESH 63ns tRC (RAS Cycle Time) * 8 refresh cycles ====> 7.5ns/cycle => 8.4 => 9 cycles * 8 refresh cycles = 56 cycles
+				IF n_s < 56 THEN
 					CKE <= '1';
 					CS <= '0';
 					RAS <= '0';
@@ -142,13 +123,11 @@ BEGIN
 					BA <= "00";
 				
 					n_s <= n_s+1;
-				ELSE
-					ram_initialized <= '1';
-					
-					CMD <= 7;
+				ELSE					
+					CMD <= 5;
 					n_s <= 0;
 				END IF;				
-			ELSIF CMD = 7 THEN -- IDLE
+			ELSIF CMD = 5 THEN -- IDLE
 				n_s <= 0;
 				
 				ram_data_save_ready <= '1'; -- ready to save
@@ -158,32 +137,32 @@ BEGIN
 					ram_data_save_ready <= '0';
 					ram_data_read_ready <= '0';
 					
-					CMD <= 8;
+					CMD <= 6;
 				END IF;		
 				IF ram_data_read_do > '1' THEN -- do save data
 					ram_data_save_ready <= '0';
 					ram_data_read_ready <= '0';
 					
-					CMD <= 9;
+					CMD <= 7;
 				END IF;			
-			ELSIF CMD = 8 THEN -- SAVE DATA
+			ELSIF CMD = 6 THEN -- SAVE DATA
 				IF n_s < 4 THEN
 					n_s <= n_s+1;
 				ELSE
 					ram_data_save_ready <= '1';
 					ram_data_read_ready <= '1';
 				
-					CMD <= 7;
+					CMD <= 5;
 					n_s <= 0;
 				END IF;
-			ELSIF CMD = 9 THEN -- READ DATA
+			ELSIF CMD = 7 THEN -- READ DATA
 				IF n_s < 4 THEN
 					n_s <= n_s+1;
 				ELSE
 					ram_data_save_ready <= '1';
 					ram_data_read_ready <= '1';
 				
-					CMD <= 7;
+					CMD <= 5;
 					n_s <= 0;
 				END IF;
 			END IF;
